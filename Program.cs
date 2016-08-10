dd26a54a5c0ecbdb67f810e828c225091b5b4d84
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2016-08-10 23:29:17 +0200

    Example of Serilog with Conole Sink.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Serilog;
+using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create Logger
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.LiterateConsole()
+                .WriteTo.RollingFile("logs/myapp-{Date}.txt")
+                .CreateLogger();
+
+            logger.Information("Hello, world!");
+
+            int a = 10, b = 0;
+            try
+            {
+                logger.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Something went wrong");
+            }
+
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+    }
+}
